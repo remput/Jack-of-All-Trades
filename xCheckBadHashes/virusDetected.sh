fc2ee9828f8e6aebb3c56f01e8eb5eb4262f90d5
remput <57086139+remput@users.noreply.github.com>
2020-02-11 10:38:46 +0800

    Update virusDetected.sh

--- xCheckBadHashes/virusDetected.sh
+++ xCheckBadHashes/virusDetected.sh
@@ -3,5 +3,5 @@ zenity --question --text="There has been a virus detected in the thumbdrive. Do 
 if [ $? = 0 ] ; then
 command=$()
 else
-command=$(udisksctl unmount -b /dev/sdc; udisksctl power-off -b /dev/sdc; zenity --error --text="You can now remove the USB" --title="USB has been unmounted.")
+command=$(udisksctl unmount -b /dev/sdb; udisksctl power-off -b /dev/sdb; zenity --error --text="You can now remove the USB" --title="USB has been unmounted.")
 fi

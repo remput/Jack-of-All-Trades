3f01dc2756a4242ffe33683017d41d828def23e9
xanderclaus00 <33832918+xanderclaus00@users.noreply.github.com>
2020-02-11 14:30:48 +0800

    Add files via upload

---
+++ JoAT/Express_Downloads/DownloadFilesSysAdmin.sh
@@ -0,0 +1,10 @@
+sudo apt install -y git
+git clone https://github.com/remput/USB-security.git
+sudo add-apt-repository ppa:unit193/encryption
+sudo apt-get -y update
+sudo apt install -y veracrypt
+sudo apt-get install -y mysql-server mysql-workbench
+sudo add-apt-repository ppa:jtaylor/keepass
+sudo apt-get update
+sudo apt-get install keepass2
+mv USB-security ~/Desktop
